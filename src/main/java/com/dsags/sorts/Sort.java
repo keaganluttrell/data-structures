8c53bd56859bbe660ba0942e4fa80dee038c7d6d
Keagan Luttrell <keaganluttrell7@gmail.com>
2021-05-18 14:15:13 -0700

    merge sort tested

--- src/main/java/com/dsags/sorts/Sort.java
+++ src/main/java/com/dsags/sorts/Sort.java
@@ -2,25 +2,27 @@ package com.dsags.sorts;
 
 public class Sort {
 
+    private static String emptyArrayMsg = "Cannot sort empty array";
+
     public static void quicksort(int[] array) {
-        if(array.length == 0) throw new EmptyArrayException("Cannot sort empty array");
+        if (array.length == 0) throw new EmptyArrayException(emptyArrayMsg);
         quicksort(array, 0, array.length - 1);
     }
 
     private static void quicksort(int[] array, int start, int end) {
-        if(start >= end) return;
+        if (start >= end) return;
         int left = start + 1;
         int right = end;
-        while(right >= left) {
-            if(array[left] > array[start] && array[right] < array[start]) {
+        while (right >= left) {
+            if (array[left] > array[start] && array[right] < array[start]) {
                 swap(array, left, right);
             }
-            if(array[left] <= array[start]) left++;
-            if(array[right] >= array[start]) right--;
+            if (array[left] <= array[start]) left++;
+            if (array[right] >= array[start]) right--;
         }
         swap(array, start, right);
         boolean LeftSubArrayIsSmaller = right - 1 - start < end - (right + 1);
-        if(LeftSubArrayIsSmaller) {
+        if (LeftSubArrayIsSmaller) {
             quicksort(array, start, right - 1);
             quicksort(array, right + 1, end);
         } else {
@@ -34,4 +36,61 @@ public class Sort {
         array[i] = array[j];
         array[j] = temp;
     }
+
+    public static void bubbleSort(int[] array) {
+        if (array.length == 0) throw new EmptyArrayException(emptyArrayMsg);
+
+        boolean swapped = true;
+        int bubbles = 1;
+
+        while (swapped) {
+            swapped = false;
+            for (int i = 0; i < array.length - bubbles; i++) {
+                if (i == array.length - bubbles) break;
+                if (array[i] > array[i + 1]) {
+                    swap(array, i, i + 1);
+                    swapped = true;
+                }
+            }
+            bubbles++;
+        }
+    }
+
+    public static void mergesort(int[] array) {
+        if(array.length == 0) throw new EmptyArrayException(emptyArrayMsg);
+        sort(array, new int[array.length], 0, array.length - 1);
+    }
+
+    private static void sort(int[] array, int[] temp, int leftStart, int rightEnd) {
+        if (leftStart >= rightEnd) return;
+        int mid = (leftStart + rightEnd) / 2;
+        sort(array, temp, leftStart, mid);
+        sort(array, temp, mid + 1, rightEnd);
+        merge(array, temp, leftStart, rightEnd);
+    }
+
+    private static void merge(int[] array, int[] temp, int leftStart, int rightEnd) {
+        int leftEnd = (rightEnd + leftStart) / 2;
+        int rightStart = leftEnd + 1;
+        int size = rightEnd - leftStart + 1;
+
+        int left = leftStart;
+        int right = rightStart;
+        int index = leftStart;
+
+        while (left <= leftEnd && right <= rightEnd) {
+            if (array[left] <= array[right]) {
+                temp[index] = array[left];
+                left++;
+            } else {
+                temp[index] = array[right];
+                right++;
+            }
+            index++;
+        }
+        System.arraycopy(array, left, temp, index, leftEnd - left + 1);
+        System.arraycopy(array, right, temp, index, rightEnd - right + 1);
+        System.arraycopy(temp, leftStart, array, leftStart, size);
+    }
+
 }
--- src/test/java/com/dsags/sorts/SortTest.java
+++ src/test/java/com/dsags/sorts/SortTest.java
@@ -48,4 +48,81 @@ public class SortTest {
         Sort.quicksort(sorted2);
         assertArrayEquals(sorted, sorted2);
     }
+
+    @Test
+    void bubbleSort_shouldExist() {
+        Sort.bubbleSort(numbers);
+    }
+
+    @Test
+    void bubbleSort_Null_Exception() {
+        assertThrows(NullPointerException.class, () -> Sort.bubbleSort(null));
+    }
+
+    @Test
+    void bubbleSort_EmptyArray_Exception() {
+        EmptyArrayException e = assertThrows(EmptyArrayException.class,
+                () -> Sort.bubbleSort(new int[0]));
+        assertEquals("Cannot sort empty array", e.getMessage());
+    }
+
+    @Test
+    void bubbleSort_SingleIndexArray_returnSameArray() {
+        int[] single = new int[]{1};
+        int[] single2 = new int[]{1};
+        Sort.bubbleSort(single);
+        assertArrayEquals(single2, single);
+    }
+
+    @Test
+    void bubbleSort_UnSortedArray_MakeArraySortedFromLowToHigh() {
+        Sort.bubbleSort(numbers);
+        assertArrayEquals(numbers, sorted);
+    }
+
+    @Test
+    void bubbleSort_SortedArray_ShouldStillBeSorted() {
+        int[] sorted2 = new int[sorted.length];
+        System.arraycopy(sorted, 0, sorted2, 0, sorted.length);
+        Sort.bubbleSort(sorted2);
+        assertArrayEquals(sorted, sorted2);
+    }
+
+    @Test
+    void mergesort_shouldExist() {
+        Sort.mergesort(numbers);
+    }
+
+    @Test
+    void mergesort_Null_Exception() {
+        assertThrows(NullPointerException.class, () -> Sort.mergesort(null));
+    }
+
+    @Test
+    void mergesort_EmptyArray_Exception() {
+        EmptyArrayException e = assertThrows(EmptyArrayException.class,
+                () -> Sort.mergesort(new int[0]));
+        assertEquals("Cannot sort empty array", e.getMessage());
+    }
+    @Test
+    void mergesort_SingleIndexArray_returnSameArray() {
+        int[] single = new int[]{1};
+        int[] single2 = new int[]{1};
+        Sort.mergesort(single);
+        assertArrayEquals(single2, single);
+    }
+
+    @Test
+    void mergesort_UnSortedArray_MakeArraySortedFromLowToHigh() {
+        Sort.mergesort(numbers);
+        assertArrayEquals(numbers, sorted);
+    }
+
+    @Test
+    void mergesort_SortedArray_ShouldStillBeSorted() {
+        int[] sorted2 = new int[sorted.length];
+        System.arraycopy(sorted, 0, sorted2, 0, sorted.length);
+        Sort.bubbleSort(sorted2);
+        assertArrayEquals(sorted, sorted2);
+    }
 }
